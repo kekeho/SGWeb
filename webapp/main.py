049deaf24be94c7f182d58735fa2b7364734d710
Hiroki.T <hirodora@me.com>
2019-07-21 01:36:42 +0900

    add: stash code to redis

--- webapp/main.py
+++ webapp/main.py
@@ -1,6 +1,18 @@
-from flask import Flask, render_template
+# Copyright (c) 2019 Hiroki Takemura (kekeho)
+# 
+# This software is released under the MIT License.
+# https://opensource.org/licenses/MIT
 
-app = Flask(__name__)
+
+from flask import Flask, render_template, request
+import redis
+import uuid
+
+
+app = Flask(__name__)  # Flask app
+app.config['SECRET_KEY'] = str(uuid.uuid4())
+
+redis_client = redis.Redis(host='redis')
 
 
 @app.route('/')
@@ -8,5 +20,13 @@ def index():
     return render_template('index.html')
 
 
+@app.route('/post_code/', methods=['POST'])
+def post_code():
+    print('get request: ', request)
+    code = request.json['code']
+    redis_client.lpush('task', code)
+    return code
+
+
 if __name__ == "__main__":
-    app.run()
+    app.run(host='0.0.0.0')
--- webapp/static/js/editor.js
+++ webapp/static/js/editor.js
@@ -1,11 +1,37 @@
+// Copyright (c) 2019 Hiroki Takemura (kekeho)
+// 
+// This software is released under the MIT License.
+// https://opensource.org/licenses/MIT
+
+// monaco editor
+var editor = null;
 require.config({
     paths: {'vs': 'https://cdnjs.cloudflare.com/ajax/libs/monaco-editor/0.17.1/min/vs'}
 });
-
 require(['vs/editor/editor.main'], function() {
-    var editor = monaco.editor.create(document.getElementById('editor'), {
+    editor = monaco.editor.create(document.getElementById('editor'), {
         language: 'shell',
         theme: 'vs-dark',
         minimap: { enabled: false }
     });
 });
+
+
+function post_shell_code(value) {
+    axios.post('/post_code', {
+        code: value
+    })
+    .then(function(response) {
+        console.log('Sent: ' + response.data);
+    })
+    .catch(function(error) {
+        console.log(error);
+    });
+}
+
+
+var run_button = document.getElementById('run_button');
+run_button.onclick = function() {
+    let value = editor.getValue();
+    post_shell_code(value);
+};
