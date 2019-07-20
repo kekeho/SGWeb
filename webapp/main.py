f79bed311cf76cf8550c0e29c271d15a6e489c65
Hiroki.T <hirodora@me.com>
2019-07-20 18:55:53 +0900

    add: navbar

--- webapp/main.py
+++ webapp/main.py
@@ -1,11 +1,11 @@
-from flask import Flask
+from flask import Flask, render_template
 
 app = Flask(__name__)
 
 
 @app.route('/')
-def hello():
-    return "Hello, World"
+def index():
+    return render_template('index.html')
 
 
 if __name__ == "__main__":
