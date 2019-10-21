e83fc366779cbdea492e086d9e574d5955ea5471
amanoese <amanoese@gmail.com>
2019-10-21 11:37:05 +0900

    images output box design

--- webapp/static/js/editor.js
+++ webapp/static/js/editor.js
@@ -48,7 +48,7 @@ function images_insert(images_list) {
         img.setAttribute('src', 'data:image/*;base64,' + b64image);
 
         let col = document.createElement('div');
-        col.classList.add('col-6');
+        col.classList.add('col');
         col.appendChild(img);
 
         image_row.appendChild(col);
